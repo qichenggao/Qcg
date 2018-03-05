1009c93c4665efb018ec4157856f1c3348e8befc
Qcg <qichenggao@126.com>
2018-03-05 03:03:54 +0000

    topic list

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories','CategoriesController',['only' => ['show'] ]);
+
