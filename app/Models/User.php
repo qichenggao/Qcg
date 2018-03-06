ebaa8fa0d31328f5fbfba47adbbf8d6ad661f2dd
Qcg <qichenggao@126.com>
2018-03-06 09:07:14 +0000

    最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,8 @@ class User extends Authenticatable
 
     use Traits\ActiveUserHelper;
 
+    use Traits\LastActivedAtHelper;
+
     use Notifiable {
         notify as protected laravelNotify;
     }
